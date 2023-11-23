5d127f3c5e8148660546ff16766d22195e2e1b15
El White <white812@purdue.edu>
2023-11-22 22:43:09 -0500

    Final

--- CGT 215 Lab 6/CGT 215 Lab 6.cpp
+++ CGT 215 Lab 6/CGT 215 Lab 6.cpp
@@ -1,13 +1,62 @@
 // CGT 215 Lab 6.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	string background = "images1/backgrounds/winter.png";// tried using the sky png but it caused issues so kept the winter.png
+	string foreground = "images1/characters/Rey_green_screen.png";
+	Texture backgroundTex;
+	if (!backgroundTex.loadFromFile(background)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+	Texture foregroundTex;
+	if (!foregroundTex.loadFromFile(foreground)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+	Image backgroundImage;
+	backgroundImage = backgroundTex.copyToImage();
+	Image foregroundImage;
+	foregroundImage = foregroundTex.copyToImage();
+	Vector2u sz = backgroundImage.getSize();
+
+	//getting the green color by checking the corner and then replacing the green foreground with the background
+
+	Color greenScreen = foregroundImage.getPixel(0, 0);
+
+	for (int y = 0; y < sz.y; y++) {
+	
+			for (int x = 0; x < sz.x; x++) {
+
+		 
+				Color example = foregroundImage.getPixel(x, y);
+				//Color example = foregroundImage.getPixel(x, y);
+				if (example == greenScreen) {
+					
+
+					Color backgroundColor = backgroundImage.getPixel(x, y);
+
+					foregroundImage.setPixel(x, y, backgroundColor);
+			}
+
+															
+			}
+	}
+
+	RenderWindow window(VideoMode(1024, 768), "This is the output");
+	Sprite sprite1;
+	Texture tex1;
+	tex1.loadFromImage(foregroundImage);
+	sprite1.setTexture(tex1);
+	window.clear();
+	window.draw(sprite1);
+	window.display();
+	while (true);
 }
-
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
